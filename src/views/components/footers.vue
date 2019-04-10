06b3898db8695896480d25c6ac0c6441ac291299
jiyiyou <983404563@qq.com>
2019-04-10 18:13:53 +0800

    l

---
+++ src/views/components/footers.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+<script>
+export default {
+    name:"footers",
+    data:{
+        return:{
+
+        }
+    }
+}
+</script>
---
+++ src/views/components/headers.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+<script>
+export default {
+    name:"headers",
+    data:{
+        return:{
+
+        }
+    }
+}
+</script>
